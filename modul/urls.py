d84c7fdab3bf8d9f3d5f9a7c0cc9c57561c3766b
Giyos1 <giyosoripov4@gmail.com>
2023-10-24 17:56:21 +0500

    add modul prodict2

--- modul/urls.py
+++ modul/urls.py
@@ -5,6 +5,6 @@ router = CustomRouter(root_view_name='modul-root')
 router.register(r'weather3-daily', views.Weather3DailyModelViewSet, basename='weather3-daily')
 router.register(r'weather24-hourly', views.Weather24HourlyModelViewSet, basename='weather24-hourly')
 router.register(r'prediction', views.PredictionModulViewSet, basename='prediction')
-router.register(r'modul', views.ModulModelViewSet, basename='modul')
+router.register(r'module', views.ModulModelViewSet, basename='module')
 
 urlpatterns = router.urls
