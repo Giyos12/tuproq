c4b7eb44b976946abad423c39477155aeb4eff01
Giyos1 <giyosoripov4@gmail.com>
2023-10-26 21:06:07 +0500

    add couter

--- modul/models.py
+++ modul/models.py
@@ -3,9 +3,13 @@ from django.utils import timezone
 from uath.models import Model
 
 
+def custom_time():
+    return timezone.now() + timezone.timedelta(hours=5)
+
+
 class Weather7Daily(models.Model):
     weather = models.JSONField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=custom_time)
 
     class Meta:
         db_table = 'weather7_daily'
@@ -15,7 +19,7 @@ class Weather7Daily(models.Model):
 
 class Weather24Hourly(models.Model):
     weather = models.JSONField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=custom_time)
 
     class Meta:
         db_table = 'weather24_hourly'
@@ -40,6 +44,7 @@ class Prediction(models.Model):
     namlik = models.FloatField()
     model = models.ForeignKey(Model, on_delete=models.DO_NOTHING, related_name='prediction')
     created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'prediction'
@@ -47,7 +52,31 @@ class Prediction(models.Model):
         verbose_name_plural = 'Predictions'
 
 
+class Counter(models.Model):
+    counter_id = models.CharField(max_length=255, unique=True)
+    b1 = models.FloatField()
+    b2 = models.FloatField()
+    b3 = models.FloatField()
+    b4 = models.FloatField()
+    b5 = models.FloatField()
+    b6 = models.FloatField()
+    b7 = models.FloatField()
+    b10 = models.FloatField()
+    gumus = models.FloatField()
+    fosfor = models.FloatField()
+    kaliy = models.FloatField()
+    shorlanish = models.FloatField()
+    namlik = models.FloatField()
+    model = models.ForeignKey(Model, on_delete=models.DO_NOTHING, related_name='counter')
+    created_at = models.DateTimeField(auto_now_add=True)
+    massiv = models.ForeignKey(Prediction, on_delete=models.DO_NOTHING, related_name='counter')
+
+    class Meta:
+        db_table = 'counter'
+        verbose_name = 'Counter'
+        verbose_name_plural = 'Counters'
+
+
 class Modul(models.Model):
     name = models.CharField(max_length=255, unique=True)
     status = models.BooleanField(default=False)
-
