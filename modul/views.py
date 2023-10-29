bc44e613e58621ea866c0da0f68c68171dd9ef8a
Giyos1 <giyosoripov4@gmail.com>
2023-10-29 15:23:46 +0500

    asd

--- modul/views.py
+++ modul/views.py
@@ -69,6 +69,19 @@ class CounterModelViewSet(ModelViewSet):
         # get last 2653 data
         return Counter.objects.all().order_by('-id')[:2653]
 
+    def list(self, request, *args, **kwargs):
+        params = request.query_params
+        if params.get('name'):
+            try:
+                p1 = Prediction.objects.get(name=params.get('name'))
+                queryset2 = self.queryset.filter(massiv=p1)
+            except Prediction.DoesNotExist:
+                return Response(data={'message': 'massiv topilmadi'}, status=404)
+            serializer = self.serializer_class(queryset2, many=True)
+            return Response(serializer.data, status=200)
+        serializer = self.serializer_class(self.get_queryset(), many=True)
+        return Response(serializer.data, status=200)
+
     def create(self, request, *args, **kwargs):
         return Response(data={'message': 'method not allowed'}, status=405)
 
