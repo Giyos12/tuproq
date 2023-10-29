32deebf3064fc2c48eb40a9e291360797019bd47
Giyos1 <giyosoripov4@gmail.com>
2023-10-29 23:24:40 +0500

    any

--- modul/models.py
+++ modul/models.py
@@ -59,7 +59,7 @@ class Prediction(models.Model):
 
 
 class Counter(models.Model):
-    counter_id = models.CharField(max_length=255, unique_for_year='date', unique_for_month='date')
+    counter_id = models.CharField(max_length=255)
     b1 = models.FloatField()
     b2 = models.FloatField()
     b3 = models.FloatField()
