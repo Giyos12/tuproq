5c46e9f20f753d4184faf6021247bdbaba6f5e9c
Giyos1 <giyosoripov4@gmail.com>
2023-10-29 19:34:41 +0500

    upt

--- tuproq/settings.py
+++ tuproq/settings.py
@@ -66,10 +66,12 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:5173',
+    'https://edaphology-admin.vercel.app'
 ]
 CORS_ALLOWED_ORIGIN_REGEXES = [
     'http://localhost:3000',
     'http://127.0.0.1:5173',
+    'https://edaphology-admin.vercel.app'
 ]
 
 ROOT_URLCONF = 'tuproq.urls'
