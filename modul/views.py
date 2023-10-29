bd036c90c4398035df11dd42684c1c665b76c6f8
Giyos1 <giyosoripov4@gmail.com>
2023-10-30 03:53:52 +0500

    any

--- modul/views.py
+++ modul/views.py
@@ -88,16 +88,24 @@ class CounterModelViewSet(ModelViewSet):
                                                    date__month=str(now_month - 1 + 12))
                 serializer = self.serializer_class(query, many=True)
                 return Response(serializer.data, status=200)
-            if params.get('monitor') == '4':
+            elif params.get('monitor') == '4':
                 now_month = int(timezone.now().month)
                 if now_month - 5 > 0:
-                    query = Counter.objects.filter(date__month=str(now_month - 5))
+                    query = Counter.objects.filter(date__year=timezone.now().year, date__month=str(now_month - 5))
                 else:
                     query = Counter.objects.filter(date__year=str(int(timezone.now().year) - 1),
                                                    date__month=str(now_month - 5 + 12))
                     serializer = self.serializer_class(query, many=True)
                     return Response(serializer.data, status=200)
-
+            elif params.get('monitor') == '1':
+                now_month = int(timezone.now().month)
+                if now_month - 1 > 0:
+                    query = Counter.objects.filter(date__year=timezone.now().year, date__month=str(now_month - 1))
+                else:
+                    query = Counter.objects.filter(date__year=str(int(timezone.now().year) - 1),
+                                                   date__month=str(now_month - 1 + 12))
+                    serializer = self.serializer_class(query, many=True)
+                    return Response(serializer.data, status=200)
         serializer = self.serializer_class(
             Counter.objects.filter(date__year=timezone.now().year, date__month=timezone.now().month), many=True)
         return Response(serializer.data, status=200)
