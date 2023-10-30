45eaa26cea79b4c81da71c8988a46fc4bb9d8e7c
Giyos1 <giyosoripov4@gmail.com>
2023-10-31 01:15:12 +0500

    add predikt namlik

--- modul/utils.py
+++ modul/utils.py
@@ -4,16 +4,17 @@ import json
 
 def namlik_predict(b5, b6):
     t = (b5 - b6) / (b5 + b6)
-    if -1 <= t < -0.6:
-        return 1
-    elif -0.6 <= t < -0.2:
-        return 2
-    elif -0.2 <= t < 0.2:
-        return 3
-    elif 0.2 <= t < 0.6:
-        return 4
-    elif 0.6 <= t <= 1:
-        return 5
+    # if -1 <= t < -0.6:
+    #     return 1
+    # elif -0.6 <= t < -0.2:
+    #     return 2
+    # elif -0.2 <= t < 0.2:
+    #     return 3
+    # elif 0.2 <= t < 0.6:
+    #     return 4
+    # elif 0.6 <= t <= 1:
+    #     return 5
+    return t
 
 
 def ml_predict(model, data):
