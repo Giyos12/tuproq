b923cd5fb4abfe8e9f06f97057b5ecbd18dcebc4
Giyos1 <giyosoripov4@gmail.com>
2023-10-30 17:15:07 +0500

    any

--- modul/service/ml.py
+++ modul/service/ml.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 def bashorat(B1, B2, B3, B4, B5, B6, B7, B10, file, filenorm):
+    print(filenorm.path)
     preprocessing = pickle.load(open(filenorm.path, 'rb'))
     model = pickle.load(open(file.path, 'rb'))
     data = preprocessing.transform([[B1, B2, B3, B4, B5, B6, B7, B10]])
