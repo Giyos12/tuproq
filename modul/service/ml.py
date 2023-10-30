4284008d49012d720f3bccaf20a2dc4074f31e2c
Giyos1 <giyosoripov4@gmail.com>
2023-10-31 02:49:30 +0500

    DL model add

--- modul/service/ml.py
+++ modul/service/ml.py
@@ -13,8 +13,8 @@ def bashorat(B1, B2, B3, B4, B5, B6, B7, B10, file, filenorm, is_dl):
         pred = model.predict(data)
         return pred
     else:
-        preprocessing = pickle.load(open(filenorm, 'rb'))
-        model = load_model(file)
+        preprocessing = pickle.load(open(filenorm.path, 'rb'))
+        model = load_model(file.path)
         data = preprocessing.transform([[B1, B2, B3, B4, B5, B6, B7, B10]])
         pred = np.argmax(np.around(model.predict(data)), axis=1) + 1
         return pred
@@ -22,9 +22,3 @@ def bashorat(B1, B2, B3, B4, B5, B6, B7, B10, file, filenorm, is_dl):
 
 
 
-def bashorat1(B1, B2, B3, B4, B5, B6, B7, B10, file, filenorm):
-    preprocessing = pickle.load(open(filenorm.path, 'rb'))
-    model = pickle.load(open(file.path, 'rb'))
-    data = preprocessing.transform([[B1, B2, B3, B4, B5, B6, B7, B10]])
-    pred = model.predict(data)
-    return pred
