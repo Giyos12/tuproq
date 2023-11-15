a8a89d894f18692eb7f111c853bd5f35f03d0334
Giyos Oripov <giyosoripov@Giyoss-MacBook-Pro.local>
2023-11-15 11:57:12 +0500

    upt

--- uath/urls.py
+++ uath/urls.py
@@ -6,5 +6,5 @@ router.register(r'auth', views.AuthModelViewSet, basename='auth')
 router.register(r'admin-system', views.ModelAdminViewSet, basename='admin-system')
 router.register(r'update-order', views.ModelOrderUpdateViewSet, basename='update-order')
 router.register(r'add-power-user', views.AddPowerUserViewSet, basename='user-add-role')
-router.register(r'expert', views.ExportCounterDBToExel, basename='expert')
+router.register(r'export', views.ExportCounterDBToExel, basename='export')
 urlpatterns = router.urls
