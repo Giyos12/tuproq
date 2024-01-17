f5442de569c5c0fff2082aae88571c889fa400f5
Giyos12 <giyosoripov4@gmail.com>
2024-01-17 19:26:03 +0500

    add private key

--- modul/apps.py
+++ modul/apps.py
@@ -7,9 +7,14 @@ class ModulConfig(AppConfig):
 
     def ready(self):
         print('Modul is ready')
-        from modul.weather_scheduling import weather_updater
-        weather_updater.start()
-        weather_updater.start2()
-        weather_updater.counter_year()
-        weather_updater.counter_season()
-        weather_updater.counter_month()
+
+        # 1 marta ishlashi uchun shart qo'shing
+        if not hasattr(ModulConfig, '_is_ready_called'):
+            ModulConfig._is_ready_called = True
+
+            from modul.weather_scheduling import weather_updater
+            weather_updater.start()
+            weather_updater.start2()
+            weather_updater.counter_year()
+            weather_updater.counter_season()
+            weather_updater.counter_month()
